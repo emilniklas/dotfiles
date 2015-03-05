4a46028b1129374dcc636d820ced6162f75d89dc
Emil Persson <emil.n.persson@gmail.com>
2015-03-05 15:12:29 +0100

    Correctly implement rupa/z

--- utilities.zsh
+++ utilities.zsh
@@ -6,3 +6,5 @@ if [[ ! -a /usr/local/bin/z  ]]; then
     wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O /usr/local/bin/z &>/dev/null
     chmod +x /usr/local/bin/z
 fi
+source /usr/local/bin/z
+
