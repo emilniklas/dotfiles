299134a45eb20be10c43c1a4d29260cf6503e182
Emil Broman <emil@emilbroman.me>
2022-01-24 19:52:41 +0100

    Add Fisher + NVM

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 alias vim=nvim
 
 export GPG_TTY=(tty)
+
+set -g fisher_path $HOME/.config/fish/fisher
+set -a fish_function_path $fisher_path/functions
--- fish/fish.mk
+++ fish/fish.mk
@@ -1,8 +1,12 @@
 FISH ?= $(BIN_DIR)/fish
+FISHER = fish/functions/fisher.fish
 
 .PHONY: fish
 fish: $(FISH)
 
-$(FISH): $(BREW)
+$(FISH): $(BREW) $(FISHER)
 	$(BREW) install fish
 	ln -his "$(PWD)/fish" $(CONFIG_DIR)/fish
+
+$(FISHER):
+	fish -c 'curl -sL https://git.io/fisher | source && fisher update'
